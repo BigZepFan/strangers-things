b27170722acef41c0e71ea04fb3a1238dbff83a1
Jaydenlundahl <jaydenlundahl20@icloud.com>
2022-07-05 14:49:16 -0600

    worked on app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,32 @@
-import { posts, profile, login } from "./components";
+import register from "components/register";
+import Login from "components/login";
+import { useEffect, useState } from "react";
+import { fetchMe } from "api/authorization";
+import Register from "components/login";
 
 export default function App() {
-  return <></>;
+  const [token, setToken] = useState("");
+  const [currentUser, setCurrentUser] = useState({});
+
+  useEffect(() => {
+    const localStorageToken = localStorage.getItem("token");
+    async function getMe() {
+      const result = await fetchMe(localStorageToken);
+      console.log("result from fetch me", result);
+      setCurrentUser(result.data);
+      setToken(localStorageToken);
+    }
+    if (localStorageToken) {
+      getMe();
+    }
+  }, [token]);
+
+  console.log("The current user is:", currentUser);
+
+  return (
+    <>
+      <Register setToken={setToken} />
+      <Login setToken={setToken} />
+    </>
+  );
 }
