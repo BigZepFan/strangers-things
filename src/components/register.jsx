a0d30fdf2a5006e7a8029e176ef6a36d664e11fe
bigzepfan <dlcandassociates@gmail.com>
2022-07-05 14:31:37 -0600

    added register

---
+++ src/components/register.jsx
@@ -0,0 +1,78 @@
+// import React, { useState } from "react";
+
+// import { registerUser } from "api/authorization";
+
+// export default function Register({setToken}){
+//     const [username, setUsername] = useState("")
+//     const [ password, setPassword]= useState ("")
+
+//     return(
+//         <div>
+//             <h4>Register</h4>
+//             <form
+//             onSubmit={async(e)=>
+//             e.preventDefault()
+//         const result = await registerUser(username,password);
+//     localStorage.setItem("token", result.data.token);
+// setToken(result.data.token);
+// setPassword("");
+// setUsername("");
+//             }}
+//             >
+//                 <input
+//                 value={username}
+//                 placeholder="username"
+//                 onChange={(e) => setUsername(e.target.value)}
+//                 />
+//                 <input
+//                 value={password}
+//                 placeholder="password"
+//                 type="password"
+//                 onChange={(e)=> setPassword(e.target.value)}
+//                 />
+//                 <button type="submit">Submit</button>
+//                 </form>
+
+//         </div>
+//     );
+// }
+
+// OURS ↑
+
+import React, { useState } from "react";
+
+import { registerUser } from "../api/authorization";
+
+export default function Register({ setToken }) {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  return (
+    <div>
+      <h4>Register:</h4>
+      <form
+        onSubmit={async (e) => {
+          e.preventDefault();
+          const result = await registerUser(username, password);
+          localStorage.setItem("token", result.data.token);
+          setToken(result.data.token);
+          setPassword("");
+          setUsername("");
+        }}
+      >
+        <input
+          value={username}
+          placeholder="username"
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <input
+          value={password}
+          placeholder="password"
+          type="password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type="submit">Submit!</button>
+      </form>
+    </div>
+  );
+}
